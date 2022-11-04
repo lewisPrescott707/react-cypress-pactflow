c4f1f10dd0bd5bf13856420f589c675e7492cd8a
prescottlewis707 <prescottlewis707@gmail.com>
2022-11-04 14:24:29 +0000

    revert last commit

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,11 @@
-const axios = require('axios')
-
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on) => {
-  on('task', {
-    getIngredients(stubUrl) {
-      const pactflow = 'https://ministryoftesting.pactflow.io/pacts/provider/Cake_API';
-      const result = axios.get(`${pactflow}/consumer/Fan_GBBO/version/0.0.3/stub${stubUrl}`,
-        {
-          headers: {
-            Authorization: 'Bearer hZyOhJLwkCFCKddqZS9nNQ',
-            Accept: "application/json; charset=UTF-8"
-          }
-        }).then(response => { 
-          return response.data })
-      return result
-    },
-  })
+
+const pactCypressPlugin = require('@pactflow/pact-cypress-adapter/dist/plugin')
+const fs = require('fs')
+
+module.exports = (on, config) => {
+  pactCypressPlugin(on, config, fs)
 }
